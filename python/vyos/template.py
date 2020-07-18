a9f97a7eb1f784af345c1dc5da06ee9358684d3e
Runar Borge <runar@borge.nu>
2020-07-18 14:58:09 +0200

    more..

--- python/vyos/template.py
+++ python/vyos/template.py
@@ -88,3 +88,36 @@ def render(destination, template, content, trim_blocks=False, formater=None, per
 
     chmod(destination, permission)
     chown(destination, user, group)
+
+def render_to_variable(template, content, trim_blocks=False, formater=None, permission=None, user=None, group=None):
+    """
+    render a template from the template directory, it will raise on any errors
+    destination: the file where the rendered template must be saved
+    template: the path to the template relative to the template folder
+    content: the dictionary to use to render the template
+
+    This classes cache the renderer, so rendering the same file multiple time
+    does not cause as too much overhead. If use everywhere, it could be changed
+    and load the template from python environement variables from an import
+    python module generated when the debian package is build
+    (recovering the load time and overhead caused by having the file out of the code)
+
+    NB! This if now a copy of render(), it needs to be rewritten so render and this uses the same generation code
+    """
+
+    # Setup a renderer for the given template
+    # This is cached and re-used for performance
+    if template not in _templates_mem[trim_blocks]:
+        _env = _templates_env[trim_blocks]
+        _env.filters['address_from_cidr'] = vyos_address_from_cidr
+        _env.filters['netmask_from_cidr'] = vyos_netmask_from_cidr
+        _templates_mem[trim_blocks][template] = _env.get_template(template)
+
+    template = _templates_mem[trim_blocks][template]
+
+    content = template.render(content)
+
+    if formater:
+        content = formater(content)
+
+    return content
--- src/conf_mode/protocols_rip.reload.py
+++ src/conf_mode/protocols_rip.reload.py
@@ -21,7 +21,7 @@ from sys import exit
 from vyos import ConfigError
 from vyos.config import Config
 from vyos.util import call
-from vyos.template import render
+from vyos.template import render_to_variable
 from vyos import frr
 from vyos import airbag
 airbag.enable()
@@ -75,6 +75,8 @@ def get_config():
 
     # Get distribute list interface 
     for dist_iface in conf.list_nodes('distribute-list interface'):
+        # TODO: The following code block is not working as expected, there are multiple format statements without {}
+
         # Set level 'distribute-list interface ethX'
         conf.set_level((str(base)) + ' distribute-list interface ' + dist_iface)
         rip_conf['rip']['distribute'].update({
@@ -85,7 +87,6 @@ def get_config():
             'iface_prefix_list_out': conf.return_value('prefix-list out'.format(dist_iface))
             }
         })
-
         # Access-list in
         if conf.exists('access-list in'.format(dist_iface)):
             rip_conf['rip']['iface_access_list_in'] = conf.return_value('access-list in'.format(dist_iface))
@@ -134,9 +135,9 @@ def get_config():
     # Get network-distance
     for net_dist in conf.list_nodes('network-distance'):
         rip_conf['rip']['net_distance'].update({
-            net_dist : {
-                'access_list' : conf.return_value('network-distance {0} access-list'.format(net_dist)),
-                'distance' : conf.return_value('network-distance {0} distance'.format(net_dist)),
+            net_dist: {
+                'access_list': conf.return_value('network-distance {0} access-list'.format(net_dist)),
+                'distance': conf.return_value('network-distance {0} distance'.format(net_dist)),
             }
         })
 
@@ -146,9 +147,9 @@ def get_config():
     # Get redistribute
     for protocol in conf.list_nodes('redistribute'):
         rip_conf['rip']['redist'].update({
-            protocol : {
-                'metric' : conf.return_value('redistribute {0} metric'.format(protocol)),
-                'route_map' : conf.return_value('redistribute {0} route-map'.format(protocol)),
+            protocol: {
+                'metric': conf.return_value('redistribute {0} metric'.format(protocol)),
+                'route_map': conf.return_value('redistribute {0} route-map'.format(protocol)),
             }
         })
 
@@ -172,6 +173,7 @@ def get_config():
 
     return rip_conf
 
+
 def verify(rip):
     if rip is None:
         return None
@@ -181,61 +183,51 @@ def verify(rip):
         if not rip['rip']['net_distance'][net]['distance']:
             raise ConfigError(f"Must specify distance for network {net}")
 
+
 def generate(rip):
     if rip is None:
         return None
 
     # As there for now are no method for rendering a template without saving it to a file
     # we save the template and rereading it to apply it
-    render(config_file, 'frr/rip.reload.frr.tmpl', rip)
+    rip['new_config'] = render_to_variable('frr/rip.reload.frr.tmpl', rip)
     return None
+
 
 def apply(rip):
     if rip is None:
         return None
 
-    if not os.path.exists(config_file):
-        raise OSError(f'File config_file not found')
-
     # Save original configration prior to starting any commit actions
     rip['original_config'] = frr.get_configuration(daemon='ripd')
 
-    # As there for now are no method for rendering a template without saving it to a file
-    # we save the template and rereading it to apply it
-    with open(config_file, 'r') as c_file:
-        repl_config = c_file.read()
-
     # Replace configuration in the original script
     # The before_re parameter is normally not needed, but because of a bug in the vyos.frr.replace_section function we need to add it.
-    new_config = frr.replace_section(rip['original_config'], repl_config, from_re='router rip', before_re=r'(line vty)')
+    modified_config = frr.replace_section(rip['original_config'], rip['new_config'], from_re='router rip', before_re=r'(line vty)')
 
     print('--------- DEBUGGING ----------')
     print(f'Existing config: \n {rip["original_config"]} \n\n')
-    print(f'Replacement config: \n {repl_config} \n\n')
-    print(f'New config: \n {new_config} \n\n')
+    print(f'Replacement config: \n {rip["new_config"]} \n\n')
+    print(f'Modified config: \n {modified_config} \n\n')
 
     # Frr Mark configuration will test for syntax errors and exception out if any syntax errors are detected prior to commiting
-    frr.mark_configuration(new_config)
+    frr.mark_configuration(modified_config)
 
     # Commit the resulting new configuration to frr, this will render an frr.CommitError() Exception on fail
-    frr.reload_configuration(new_config, daemon='ripd')
-
-    # call(f'vtysh -d ripd -f {config_file}')
-    # as part of debugging we do not remove the config file after its applied
-    # os.remove(config_file)
+    frr.reload_configuration(modified_config, daemon='ripd')
 
     return None
 
 
 def rollback(rip):
     if 'original_config' not in rip:
-        print('Applying configuration failed and there were nothing to roll back to')
+        print('Applying old configuration failed and there were nothing to roll back to')
         return None
-
+    print('Trying to execute rollback operation')
     try:
         frr.reload_configuration(rip['original_config'], daemon='ripd')
     except frr.CommitError as e:
-        print('Failed to reapply old configuration')
+        print('Failed to reapply old configuration:')
         print(e)
 
     return None
@@ -249,10 +241,10 @@ if __name__ == '__main__':
         try:
             apply(c)
         except frr.CommitError as e:
-            print('Commit error, Rolling back')
+            print('Commit error:')
             print(e)
             rollback(c)
-            raise
+            exit(1)
     except ConfigError as e:
         print(e)
         exit(1)
